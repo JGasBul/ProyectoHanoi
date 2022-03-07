07a5a776ae7bc60dffbc5193a1fbebda342d4caf
Pepe Gascó Bule <pepegascobule@gmail.com>
2022-03-07 17:05:44 +0100

    I upload all the progress of the class of the day 07/03/22. Implemented Stack class.

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -8,35 +8,47 @@ namespace Torres_de_Hanoi
 {
     class Pila
     {
+        //Declaramos Variables.
         public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
         public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
         public List<Disco> Elementos { get; set; }
-        */
 
-        /* TODO: Implementar métodos */
-        public Pila()
+        //Constructor Vacio
+        public Pila(){}
+        //Constructor Pila
+        public Pila(int Size)
         {
+            this.Size = Size;
+            Elementos = new List<Disco>();
+            Top = -1;
 
+            for (int i = this.Size; i > 0; i--)
+            {
+                Elementos.Add(new Disco(i));
+                Top = Elementos.Last().Valor;
+            }
         }
-
+        
+        //Metodo para añadir un disco a la pila
         public void push(Disco d)
         {
-
+            Elementos.Add(d);
+            Top = Elementos.Last().Valor;
         }
-
+        
+        //Metodo para eliminar un disco de la pila
         public Disco pop()
         {
-            return null;
-        }                
+            Disco d = Elementos.Last();
+            Elementos.Remove(d);
+            Top = Elementos.Last().Valor;
+            return d;
+        }
 
+        //Metodo para saber si la lista elementos esta vacia.
         public bool isEmpty()
         {
-            return true;
+            return !Elementos.Any();
         }
 
     }
